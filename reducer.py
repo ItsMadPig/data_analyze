95e3042c5d3e09af046518d561e202950ac192bc
Aaron Hsu <ahsu1@andrew.cmu.edu>
2015-01-30 18:47:56 -0500

    updated reducer.py

--- reducer.py
+++ reducer.py
@@ -27,7 +27,18 @@ for line in sys.stdin:
 		current_outline = ""
 		for x in range(1,31):
 			current_outline += "\t"+ "201411" + str(x).rjust(2,'0')+":0"
-	current_outline = current_outline.replace((date+":0"),(date+':'+str(count)))
+	idx1 = current_outline.index(date)
+	idx2 = current_outline.index(":",idx1+1)
+	try:
+		idx3 = current_outline.index("\t",idx1+1)
+		c = int(current_outline[idx2+1:idx3])
+		current_outline = current_outline[:idx2+1] + str(c+count)+ current_outline[idx3:]
+	except ValueError:
+		idx3 = len(current_outline)
+		c = int(current_outline[idx2+1:idx3])
+		current_outline = current_outline[:idx2+1] + str(c+count)
+
+	#current_outline = current_outline.replace((date+":0"),(date+':'+str(count)))
 if current_word == word:
 	if current_count > 100000:
 		print '%s\t%s%s' %(current_count,current_word,current_outline)
